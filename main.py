5507bda0f2830967146dcdedd87c1eaa0da3f599
AlikoGrace <gracealiko09@gmail.com>
2026-04-03 22:37:11 +0000

    extact URL path from  request line and return 200 or 404 based on path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import socket
 
+
 def main():
 
     server_socket=socket.create_server(("localhost", 4221), reuse_port=True)
@@ -7,7 +8,21 @@ def main():
 
     print(f"connected successfully with {address}")
 
+    data=connection.recv(1024)
 
+    request_line=data.split(b"\r\n")[0]
+    parts=request_line.split(b" ")
+    path=parts[1]
+
+    if path == (b"/"):
+        connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+
+    else:
+        connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+  
+
+
+    connection.close()   
 
 
 
